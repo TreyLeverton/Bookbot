8bd298bbf04199e47e2ef6dfaaf868f220c60edd
SaggyFlapJax <treyleverton@yahoo.com>
2025-04-03 14:19:45 -0500

    refactored the main function to take multiple book inputs

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from stats import count_words
 from stats import char_count
 from stats import sort_chars
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = count_words(text)
     chars_dict = char_count(text)
